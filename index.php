1648bd23046b74664db63d2b7422bcbb73b96da7
spencerbyw <spencer.bywater@gmail.com>
2015-02-19 20:21:00 -0700

    Adding index.php
    Created a basic index.php file to get this project going.

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="Content-Type" content="text/html;utf-8">
+	<title>Blogwater</title>
+</head>
+<body>
+	<h1>Blogwater</h1>
+	<p>Spencer A. Bywater's blog</p>
+
+	<h2>Article 1 title</h2>
+	<div>dd Mon YYYY</div>
+	<p>A paragraph summarizing article 1.</p>
+	<p>
+		<a href="#">Read more.</a>
+	</p>
+
+	<h2>Article 2 title</h2>
+	<div>dd Mon YYYY</div>
+	<p>A paragraph summarizing article 2.</p>
+	<p>
+		<a href="#">Read more.</a>
+	</p>
+</body>
+</html>
